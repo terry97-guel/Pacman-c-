a9122b3c7e36ed5ff2092c85492b2b085979dd9e
terry <taerimyoon@korea.ac.kr>
2023-07-15 18:44:12 +0900

    testMap

--- GameManager.h
+++ GameManager.h
@@ -48,11 +48,12 @@ void EnemyState::PrintInfo(int EnemyIdx){
 
 class Env{
     public:
-        // GUI* gui;
+        // HandlerGUI* gui;
         UserState* userState;
         EnemyState* enemyStates;
         int numEnemy;
         Env(int numEnemy);
+        ~Env();
         void PrintInfo();
 };
 
@@ -73,3 +74,92 @@ void Env::PrintInfo(){
     }
 }
 
+Env::~Env(){
+    delete userState;
+    delete[] enemyStates;
+}
+
+
+class HandlerGUI{
+    public:
+        sf::RenderWindow* window;
+    	sf::Sprite mapSprite;
+        sf::Texture mapTexture;
+        float aspectRatio;
+
+        Env* env;
+        HandlerGUI(Env* env);
+        ~HandlerGUI();
+        void Draw();
+
+    private:
+        void LoadTextures();
+};
+
+HandlerGUI::HandlerGUI(Env* env): env(env){
+    window = new sf::RenderWindow(sf::VideoMode(200, 200), "SFML works!");
+    // window->create(sf::VideoMode(1000, 1100), "Pacman", sf::Style::Fullscreen);
+    // aspectRatio = float(window->getSize().x) / float(window->getSize().y);
+    // sf::View v(sf::Vector2f(400, 450), sf::Vector2f(800 * aspectRatio, 900));
+    // window->setView(v);
+
+    LoadTextures();
+    
+}
+
+HandlerGUI::~HandlerGUI(){
+    delete window;
+}
+
+void HandlerGUI::LoadTextures(){
+
+    if (mapTexture.loadFromFile("Resources/PacManSprites.png", sf::IntRect(0, 0, 226, 248)))
+    {
+        mapTexture.setSmooth(false);
+        mapSprite.setTexture(mapTexture);
+        // mapSprite.setScale((window->getView().getSize().x) / (mapSprite.getLocalBounds().width * aspectRatio), (window->getView().getSize().y - 100) / mapSprite.getLocalBounds().height);
+        mapSprite.move(0, 1);
+    }
+    else
+    {
+        std::cout << "texture not loaded correctly" << std::endl;
+    }
+
+}
+
+void HandlerGUI::Draw(){
+    window->clear();
+    window->draw(mapSprite);
+    window->display();
+}
+
+
+class GameManager{
+    public:
+        Env* env;
+        HandlerGUI* handlerGUI;
+        GameManager();
+        ~GameManager();
+        void Run();
+
+    private:
+        sf::Clock clock;
+};
+
+
+GameManager::GameManager(){
+    int enemyNum = 10;
+    env = new Env(enemyNum);
+    handlerGUI = new HandlerGUI(env);
+}
+
+GameManager::~GameManager(){
+    delete env;
+    delete handlerGUI;
+}
+
+void GameManager::Run(){
+    while (handlerGUI->window->isOpen()){
+        handlerGUI->Draw();
+    }
+}
